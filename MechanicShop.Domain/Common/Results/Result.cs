d81603e811c928dad98fbd7e33942b274a52a807
Abdallah0592330273 <126710474+Abdallah0592330273@users.noreply.github.com>
2026-07-13 20:22:18 +0300

    Common file in domain
    finishing common files as primary work with including result pattern and basic entites and events

---
+++ MechanicShop.Domain/Common/Results/Result.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MechanicShop.Domain.Common.Results.Interface
+{
+    internal class Result
+    {
+    }
+}
---
+++ MechanicShop.Domain/Common/SoftDeletableEntity.cs
@@ -0,0 +1,39 @@
+﻿namespace MechanicShop.Domain.Common.Entities;
+
+public abstract class SoftDeletableEntity : AuditableEntity
+{
+    protected SoftDeletableEntity(Guid id) : base(id)
+    {
+    }
+
+    protected SoftDeletableEntity()
+    {
+        // For EF Core
+    }
+
+    public bool IsDeleted { get; private set; }
+
+    public DateTimeOffset? DeletedAtUtc { get; private set; }
+
+    public Guid? DeletedByUserId { get; private set; }
+
+    public void SoftDelete(DateTimeOffset utcNow, Guid? userId)
+    {
+        if (IsDeleted)
+            return;
+
+        IsDeleted = true;
+        DeletedAtUtc = utcNow;
+        DeletedByUserId = userId;
+    }
+
+    public void Restore()
+    {
+        if (!IsDeleted)
+            return;
+
+        IsDeleted = false;
+        DeletedAtUtc = null;
+        DeletedByUserId = null;
+    }
+}
